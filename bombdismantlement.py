f89c9baff295b752d0e1d133c1d0e2f69f404a81
bc10201 <bmym2010xh2@gmail.com>
2022-10-05 12:08:30 +0800

    更新UI，0-9输入逻辑完成

--- bombdismantlement.py
+++ bombdismantlement.py
@@ -8,10 +8,52 @@ class BombDis(QMainWindow,Ui_Bombdismantlement):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.password_ = ""
+        self.password.setDigitCount(10)
+        self.password.setEnabled(False)
+        self.one.clicked.connect(self.one_add)
+        self.two.clicked.connect(self.two_add)
+        self.three.clicked.connect(self.three_add)
+        self.four.clicked.connect(self.four_add)
+        self.five.clicked.connect(self.five_add)
+        self.six.clicked.connect(self.six_add)
+        self.seven.clicked.connect(self.seven_add)
+        self.eight.clicked.connect(self.eight_add)
+        self.nine.clicked.connect(self.nine_add)
+        self.zero.clicked.connect(self.zero_add)
         self.show()
-    
-
-
+    def one_add(self):
+        self.password_=self.password_+"1"
+        self.update_password()
+    def two_add(self):
+        self.password_=self.password_+"2"
+        self.update_password()
+    def three_add(self):
+        self.password_=self.password_+"3"
+        self.update_password()
+    def four_add(self):
+        self.password_=self.password_+"4"
+        self.update_password()
+    def five_add(self):
+        self.password_=self.password_+"5"
+        self.update_password()
+    def six_add(self):
+        self.password_=self.password_+"6"
+        self.update_password()
+    def seven_add(self):
+        self.password_=self.password_+"7"
+        self.update_password()
+    def eight_add(self):
+        self.password_=self.password_+"8"
+        self.update_password()
+    def nine_add(self):
+        self.password_=self.password_+"9"
+        self.update_password()
+    def zero_add(self):
+        self.password_=self.password_+"0"
+        self.update_password()
+    def update_password(self):
+        self.password.display(self.password_)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = BombDis()
